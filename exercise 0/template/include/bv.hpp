00253dbb7812b08f35767b1d6e09200f1876b865
ville03 <ville.r.p.parkkinen@gmail.com>
2025-03-13 03:53:56 +0200

    inital commit

---
+++ exercise 0/template/include/bv.hpp
@@ -0,0 +1,21 @@
+/**
+ * You can use this for programming task 2 of set 1 
+*/
+
+#pragma once
+
+namespace pfp {
+
+template <class dtype>
+class bv {
+  public:
+    bv(dtype limit) {}
+
+    void insert(dtype value) { }
+
+    int count(dtype value) const {
+        return 0;
+    }
+};
+
+} // namespace pfp
---
+++ exercise 0/template/include/vs.hpp
@@ -0,0 +1,19 @@
+/**
+ * You can use this file for programmign task 1 of set 1.
+*/
+
+#pragma once
+
+namespace pfp {
+
+template <class dtype>
+class vs {
+  public:
+    void insert(dtype val) { }
+
+    int count(dtype val) {
+        return 0;
+    }
+};
+
+}  // namespace pfp
